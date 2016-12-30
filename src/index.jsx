1bf3cb9b1e86b8085f359076c0a11b05d6d7c496
zccz14 <zccz14@outlook.com>
2016-12-31 05:35:59 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,37 @@ import ReactDOM from 'react-dom';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import {
+    cyan500, cyan700,
+    pinkA200,
+    lightBlue300,
+    grey100, grey300, grey400, grey500,
+    white, darkBlack, fullBlack,
+} from 'material-ui/styles/colors';
+import { fade } from 'material-ui/utils/colorManipulator';
+
+const muiTheme = getMuiTheme({
+    fontFamily: 'Roboto, sans-serif',
+    palette: {
+        primary1Color: lightBlue300,
+        primary2Color: cyan700,
+        primary3Color: grey400,
+        accent1Color: pinkA200,
+        accent2Color: grey100,
+        accent3Color: grey500,
+        textColor: darkBlack,
+        alternateTextColor: white,
+        canvasColor: white,
+        borderColor: grey300,
+        disabledColor: fade(darkBlack, 0.3),
+        pickerHeaderColor: cyan500,
+        clockCircleColor: fade(darkBlack, 0.07),
+        shadowColor: fullBlack,
+    },
+});
 
 const App = () => (
-    <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <AppBar title="Hello, Material-UI!" />
     </MuiThemeProvider>
 );
